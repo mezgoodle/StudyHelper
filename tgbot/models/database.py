af904a2cf883979a04f509f47d6032d57919f319
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-02-07 15:07:50 +0200

    Start creating class of the db

--- tgbot/models/database.py
+++ tgbot/models/database.py
@@ -1,7 +1,15 @@
 from typing import Optional
 
 from sqlalchemy.future.engine import Engine
-from sqlmodel import Field, SQLModel, create_engine
+from sqlmodel import Field, Session, SQLModel, create_engine
+
+
+class Database:
+    def __init__(self, sqlite_file_name: str = "database.db"):
+        sqlite_url = f"sqlite:///{sqlite_file_name}"
+        self.engine = create_engine(sqlite_url, echo=True)
+        self.session = Session(self.engine)
+        SQLModel.metadata.create_all(self.engine)
 
 
 class Hero(SQLModel, table=True):
@@ -9,25 +17,3 @@ class Hero(SQLModel, table=True):
     name: str
     secret_name: str
     age: Optional[int] = None
-
-
-def create_db_engine(sqlite_file_name: str = "database.db") -> Engine:
-    """Function to create sqlmodel engine
-
-    Args:
-        sqlite_file_name (str, optional): Path to the database. Defaults to "database.db".
-
-    Returns:
-        Engine: engine
-    """
-    sqlite_url = f"sqlite:///{sqlite_file_name}"
-    return create_engine(sqlite_url, echo=True)
-
-
-def create_db_and_tables(engine: Engine):
-    """Funtion to create tables in db with the engine
-
-    Args:
-        engine (Engine): engine instance
-    """
-    SQLModel.metadata.create_all(engine)
