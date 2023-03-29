f59677aa6b45d9a840babfbfbb2feff8e786c1a4
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-03-29 09:21:25 +0300

    CU-861mjrnx1 Install pandas

--- tgbot/misc/import_students.py
+++ tgbot/misc/import_students.py
@@ -1,7 +1,15 @@
 from pathlib import Path
 
 from aiogram.types import Message
+from pandas import read_csv, read_excel
+
+FILE_EXTENSIONS = {
+    (".csv",): read_csv,
+    (".xlsx", ".xls"): read_excel,
+}
 
 
 def parse_students_from_file(path: Path):
-    pass
+    extension = path.suffix
+    df = FILE_EXTENSIONS.get(extension, read_csv)(path)
+    print(df)
