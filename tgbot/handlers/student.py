4179cefd71ca139fb8fa020f7f4b161722c4e902
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-04-19 10:39:18 +0300

    CU-85zrv0x7y Registrate students with new state and keyboard

--- tgbot/handlers/student.py
+++ tgbot/handlers/student.py
@@ -3,17 +3,29 @@ from aiogram.dispatcher.filters import Command
 from aiogram.types import Message
 
 from loader import dp
+from tgbot.keyboards.reply.subjects import create_markup
 from tgbot.models.database import Database
+from tgbot.states.states import Student
 
 
 @dp.message_handler(Command(["register_student"]))
-async def register_student(message: Message, state: FSMContext) -> Message:
-    await state.set_state("wait_for_group")
+async def register_student(message: Message) -> Message:
+    await Student.first()
     return await message.answer("Send your group")
 
 
-@dp.message_handler(state="wait_for_group")
+@dp.message_handler(state=Student.group)
 async def process_group(message: Message, state: FSMContext) -> Message:
+    db: Database = message.bot.get("db")
+    subjects = db.get_subjects()
+    keyboard = create_markup(subjects)
+    await state.update_data(group=message.text)
+    await Student.next()
+    return await message.answer("Choose your subject", reply_markup=keyboard)
+
+
+@dp.message_handler(state=Student.subject)
+async def process_subject(message: Message, state: FSMContext) -> Message:
     await state.finish()
     db: Database = message.bot.get("db")
     user = message.from_user
