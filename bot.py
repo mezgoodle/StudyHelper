8d29c114ce6a0fb42fa61a7f9b004da8b16f6751
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-09-07 10:37:59 +0300

    Add logger and on_shutdown hook

--- bot.py
+++ bot.py
@@ -14,17 +14,41 @@ from loader import bot, dp
 def register_all_handlers() -> None:
     from tgbot import handlers
 
+    logging.info("Handlers registered.")
+
+
+def register_all_middlewares() -> None:
+    logging.info("Middlewares registered.")
+
+
+def register_all_filters() -> None:
+    logging.info("Filters registered.")
+
 
 async def on_startup() -> None:
     register_all_handlers()
+    register_all_middlewares()
+    register_all_filters()
+
+
+async def on_shutdown(dispatcher: Dispatcher) -> None:
+    await dispatcher.storage.close()
+    logging.info("Storage closed.")
+    logging.info("Bot stopped.")
 
 
 async def main() -> None:
     dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
     # And the run events dispatching
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    logging.basicConfig(
+        level=logging.INFO,
+        filename="bot.log",
+        format="%(asctime)s :: %(levelname)s :: %(module)s.%(funcName)s :: %(lineno)d :: %(message)s",
+        filemode="w",
+    )
     asyncio.run(main())
