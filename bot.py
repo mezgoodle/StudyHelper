7bb46a3bbfaefa97e30c21fc17d330b380810fc5
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-09-08 16:46:16 +0300

    Add services scripts

--- bot.py
+++ bot.py
@@ -1,19 +1,26 @@
 import asyncio
 import logging
 
-from aiogram import Dispatcher
+from aiogram import Bot, Dispatcher
 from aiogram.utils.callback_answer import CallbackAnswerMiddleware
 
 from loader import bot, dp
 from tgbot.config import Settings, config
 from tgbot.middlewares.settings import ConfigMiddleware
 from tgbot.middlewares.throttling import ThrottlingMiddleware
+from tgbot.services.admins_notify import on_startup_notify
+from tgbot.services.setting_commands import set_default_commands
 
 
 def register_all_handlers() -> None:
     from tgbot import handlers  # noqa: F401
 
     logging.info("Handlers registered.")
+
+
+async def register_all_commands(bot: Bot) -> None:
+    await set_default_commands(bot)
+    logging.info("Commands registered.")
 
 
 def register_global_middlewares(dp: Dispatcher, config: Settings):
@@ -30,11 +37,15 @@ def register_global_middlewares(dp: Dispatcher, config: Settings):
     dp.callback_query.middleware(
         CallbackAnswerMiddleware(pre=True, text="Ready!", show_alert=True)
     )
+    logging.info("Middlewares registered.")
 
 
-async def on_startup(dispatcher: Dispatcher) -> None:
+async def on_startup(bot: Bot, dispatcher: Dispatcher) -> None:
     register_all_handlers()
     register_global_middlewares(dispatcher, config)
+    await register_all_commands(bot)
+    await on_startup_notify(bot)
+    logging.info("Bot started.")
 
 
 async def on_shutdown(dispatcher: Dispatcher) -> None:
@@ -48,6 +59,7 @@ async def main() -> None:
     dp.shutdown.register(on_shutdown)
     # And the run events dispatching
     await dp.start_polling(bot)
+    # * For the webhook usage: https://docs.aiogram.dev/en/dev-3.x/dispatcher/webhook.html#examples
 
 
 if __name__ == "__main__":
