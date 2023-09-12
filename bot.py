ecf640d3fe430baeeb21ed25f9bc662845b95661
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-09-12 10:52:05 +0300

    Set up models and scripts for db

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ from loader import bot, dp
 from tgbot.config import Settings, config
 from tgbot.middlewares.settings import ConfigMiddleware
 from tgbot.middlewares.throttling import ThrottlingMiddleware
+from tgbot.models.models import close_db, init
 from tgbot.services.admins_notify import on_startup_notify
 from tgbot.services.setting_commands import set_default_commands
 
@@ -40,9 +41,15 @@ def register_global_middlewares(dp: Dispatcher, config: Settings):
     logging.info("Middlewares registered.")
 
 
+async def init_database():
+    await init()
+    logging.info("Database was inited")
+
+
 async def on_startup(bot: Bot, dispatcher: Dispatcher) -> None:
     register_all_handlers()
     register_global_middlewares(dispatcher, config)
+    await init_database()
     await register_all_commands(bot)
     await on_startup_notify(bot)
     logging.info("Bot started.")
@@ -51,6 +58,8 @@ async def on_startup(bot: Bot, dispatcher: Dispatcher) -> None:
 async def on_shutdown(dispatcher: Dispatcher) -> None:
     await dispatcher.storage.close()
     logging.info("Storage closed.")
+    await close_db()
+    logging.info("Database was closed.")
     logging.info("Bot stopped.")
 
 
