ee03bebdadd6b814a3b3bc6eeaf93442729fba4e
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-09-25 19:57:26 +0300

    Write models

--- tgbot/models/models.py
+++ tgbot/models/models.py
@@ -1,44 +1,93 @@
 from tortoise import Tortoise, fields
-from tortoise.models import Model
+
+from tgbot.models.base import TimedBaseModel
 
 db = Tortoise()
 
 
-class Tournament(Model):
-    # Defining `id` field is optional, it will be defined automatically
-    # if you haven't done it yourself
-    id = fields.IntField(pk=True)
-    name = fields.CharField(max_length=255)
-
-    # Defining ``__str__`` is also optional, but gives you pretty
-    # represent of model in debugger and interpreter
-    def __str__(self):
-        return self.name
-
-
-class Event(Model):
-    id = fields.IntField(pk=True)
-    name = fields.CharField(max_length=255)
-    # References to other models are defined in format
-    # "{app_name}.{model_name}" - where {app_name} is
-    # defined in tortoise config
-    tournament = fields.ForeignKeyField(
-        "models.Tournament", related_name="events"
+class User(TimedBaseModel):
+    name = fields.CharField(max_length=255, null=True, description="User name")
+    user_id = fields.IntField(
+        unique=True,
+        description="Telegram user id",
+        index=True,
     )
-    participants = fields.ManyToManyField(
-        "models.Team", related_name="events", through="event_team"
+    username = fields.CharField(
+        max_length=255,
+        null=True,
+        description="Telegram username",
+        unique=True,
+    )
+    subjects: fields.ManyToManyRelation["Subject"]
+
+    class Meta:
+        abstract = True
+
+
+class Teacher(User):
+    pass
+
+
+class Student(User):
+    completed_tasks: fields.ReverseRelation["CompletedTask"]
+    uncompleted_tasks: fields.ReverseRelation["UncompletedTask"]
+
+
+class Subject(TimedBaseModel):
+    name = fields.CharField(max_length=255, description="Subject name")
+    description = fields.TextField(null=True)
+    teacher: fields.ForeignKeyRelation[Teacher] = fields.ForeignKeyField(
+        "models.Teacher",
+        related_name="subjects",
+        description="Subject teacher",
+    )
+    students: fields.ManyToManyRelation[Student] = fields.ManyToManyField(
+        "models.Student",
+        related_name="subjects",
+        description="Subject students",
+    )
+    drive_link = fields.CharField(
+        max_length=255,
+        null=True,
+        description="Drive link",
+    )
+    tasks: fields.ReverseRelation["Task"]
+
+    def __str__(self):
+        return self.name
+
+
+class Task(TimedBaseModel):
+    name = fields.CharField(max_length=255)
+    description = fields.TextField()
+    subject: fields.ForeignKeyRelation[Subject] = fields.ForeignKeyField(
+        "models.Subject", related_name="tasks", description="Task subject"
+    )
+    due_date = fields.DatetimeField(
+        null=True,
     )
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        abstract = True
 
-class Team(Model):
-    id = fields.IntField(pk=True)
-    name = fields.CharField(max_length=255)
 
-    def __str__(self):
-        return self.name
+class UncompletedTask(Task):
+    student: fields.ForeignKeyRelation[Student] = fields.ForeignKeyField(
+        "models.Student",
+        related_name="uncompleted_tasks",
+        description="Task student",
+    )
+
+
+class CompletedTask(Task):
+    student: fields.ForeignKeyRelation[Student] = fields.ForeignKeyField(
+        "models.Student",
+        related_name="completed_tasks",
+        description="Task student",
+    )
 
 
 async def init():
