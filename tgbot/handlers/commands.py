bb0d8835b28dcaca8998120471c1cabeb0127bb4
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-10-25 19:45:49 +0300

    CU-8692yxmhr_Add-students-to-the-subjects (#17)
    * Create deep_links for inviting students to the subjects
    
    * Add student to the subject
    
    * Small changes

--- tgbot/handlers/commands.py
+++ tgbot/handlers/commands.py
@@ -1,9 +1,13 @@
+from json import loads
+
 from aiogram import Router
-from aiogram.filters import Command
+from aiogram.filters import Command, CommandObject, CommandStart
 from aiogram.types import Message
+from aiogram.utils.deep_linking import decode_payload
 
 from loader import dp
 from tgbot.misc.database import Database
+from tgbot.misc.utils import utils
 
 router = Router()
 dp.include_router(router)
@@ -33,3 +37,15 @@ async def register_as_student(message: Message, db: Database) -> None:
             "You are registered as a student. To check it, type /is_student"
         )
     return await message.answer("Error while registering as a student")
+
+
+@router.message(CommandStart(deep_link=True))
+async def handler(
+    message: Message, command: CommandObject, db: Database
+) -> Message:
+    args = command.args
+    payload = decode_payload(args)
+    data: dict = loads(payload)
+    if (key := data.get("key")) and key in utils:
+        result = await utils.get(key)(message, data, db)
+        return await message.answer(result)
