b58f69ccb2880b0a39dfba781fafa260d5d9f227
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-10-31 14:48:55 +0200

    Add method for deleting relation from the subject

--- tgbot/misc/database.py
+++ tgbot/misc/database.py
@@ -13,17 +13,6 @@ class Database:
         username: str | None = None,
         name: str | None = None,
     ):
-        """
-        Creates a teacher with the given user ID, username, and name.
-
-        Parameters:
-            user_id (int): The ID of the user.
-            username (str | None, optional): The username of the teacher. Defaults to None.
-            name (str | None, optional): The name of the teacher. Defaults to None.
-
-        Returns:
-            The created teacher object.
-        """  # noqa: E501
         return await self.__create_user(
             self.teacher, user_id=user_id, username=username, name=name
         )
@@ -34,18 +23,6 @@ class Database:
         username: str | None = None,
         name: str | None = None,
     ):
-        """
-        Create a new student record.
-
-        Args:
-            user_id (int): The ID of the user associated with the student.
-            username (str, optional): The username of the student.
-                Defaults to None.
-            name (str, optional): The name of the student. Defaults to None.
-
-        Returns:
-            The created student object.
-        """
         return await self.__create_user(
             self.student, user_id=user_id, username=username, name=name
         )
@@ -55,38 +32,11 @@ class Database:
         db_object: Student | Teacher,
         **kwargs,
     ):
-        """
-        Creates a new user in the database.
-
-        Args:
-            db_object (Student | Teacher): The database object
-                used to create the user.
-            *args: Additional arguments passed to the create method.
-
-        Returns:
-            The result of the create method.
-        """
         return await db_object.create(**kwargs)
 
     async def create_subject(
         self, name: str, description: str, drive_link: str, teacher_id: int
     ) -> Subject:
-        """
-        Creates a new subject with the given name, description, drive link,
-        and teacher ID.
-
-        Args:
-            name (str): The name of the subject.
-            description (str): The description of the subject.
-            drive_link (str): The Google Drive link associated with the subject.
-            teacher_id (int): The ID of the teacher responsible for the subject.
-
-        Returns:
-            Subject: The newly created Subject object.
-
-        Raises:
-            None
-        """
         return await self.subject.create(
             name=name,
             description=description,
@@ -95,83 +45,24 @@ class Database:
         )
 
     def get_teachers(self) -> list[Teacher]:
-        """
-        Get the list of teachers.
-
-        :return: A list of Teacher objects.
-        """
         return self.teacher.all()
 
     def get_students(self) -> list[Student]:
-        """
-        Returns a list of all students.
-
-        :return: A list of Student objects representing all the students.
-        :rtype: list[Student]
-        """
         return self.student.all()
 
     def get_teacher(self, user_id: int) -> Teacher | None:
-        """
-        Retrieves a teacher based on the provided user ID.
-
-        Parameters:
-            user_id (int): The ID of the user for which to retrieve the teacher.
-
-        Returns:
-            Teacher | None: The teacher object corresponding to the provided user ID,
-            or None if no teacher is found.
-        """
         return self.teacher.filter(user_id=user_id).first()
 
     def get_subject(self, subject_id: int) -> Subject | None:
-        """
-        Retrieves a subject based on the provided subject ID.
-
-        Parameters:
-            subject_id (int): The ID of the subject for which to retrieve the subject.
-
-        Returns:
-            Subject | None: The subject object corresponding to the provided subject ID,
-            or None if no subject is found.
-        """
         return self.subject.filter(id=subject_id).first()
 
     def get_student(self, user_id: int) -> Student | None:
-        """
-        Retrieves a student based on the provided user ID.
-
-        Parameters:
-            user_id (int): The ID of the user for which to retrieve the student.
-
-        Returns:
-            Student | None: The student object corresponding to the provided user ID,
-            or None if no student is found.
-        """
         return self.student.filter(user_id=user_id).first()
 
     async def get_subjects_by_teacher_id(
         self, teacher_id: int
     ) -> list[Subject]:
-        """
-        Retrieves a list of subjects based on a given teacher ID.
-
-        Args:
-            teacher_id (int): The ID of the teacher.
-
-        Returns:
-            list[Subject]: A list of Subject objects filtered by the given teacher ID.
-        """
         return await self.subject.filter(teacher_id=teacher_id).all()
 
     async def get_student_subjects(self, student: Student) -> list[Subject]:
-        """
-        Retrieves a list of subjects based on a given student ID.
-
-        Args:
-            student (Student): The Student object
-
-        Returns:
-            list[Subject]: A list of Subject objects filtered by the given student ID.
-        """
         return await student.subjects
