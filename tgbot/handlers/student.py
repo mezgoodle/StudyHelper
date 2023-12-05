9b7b75767b1beed96e447494b9a4e995cba0762e
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-12-05 17:21:37 +0200

    Fix lint error

--- tgbot/handlers/student.py
+++ tgbot/handlers/student.py
@@ -1,6 +1,5 @@
 from aiogram import F, Router
 from aiogram.filters import Command
-from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery, Message
 from aiogram.utils.markdown import hbold
 
