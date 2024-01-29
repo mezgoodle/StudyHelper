ebad877041ca90f3edbd380f4c859295fe16964b
Maksym Zavalniuk <mezgoodle@gmail.com>
2024-01-29 20:10:19 +0200

    Remove unnecessary code

--- tgbot/handlers/echo.py
+++ tgbot/handlers/echo.py
@@ -1,4 +1,4 @@
-from aiogram import F, Router
+from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
@@ -13,17 +13,3 @@ dp.include_router(router)
 @router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
-
-
-@router.message(F.photo)
-async def photo_msg(message: Message):
-    await message.answer("This is image!")
-
-
-@router.message()
-async def echo_handler(message: Message, config: Settings) -> None:
-    try:
-        print(config)
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        await message.answer("Nice try!")
