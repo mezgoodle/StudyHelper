25bd6bcbc87a4f6cfa7773425d34b7e9f75c9ca9
Maksym Zavalniuk <mezgoodle@gmail.com>
2024-01-29 19:43:49 +0200

    Back teacher buttons

--- tgbot/keyboards/inline/task_keyboard.py
+++ tgbot/keyboards/inline/task_keyboard.py
@@ -19,11 +19,11 @@ async def task_keyboard(
         solution := await db.get_student_solution(user_id, task_id)
     ):
         buttons_list[2]["text"] = f"Your grade: {solution.grade}"
-    # if await db.is_teacher(user_id):
-    #     buttons_list = [
-    #         {"text": "See solutions", "action_text": "show_solutions"},
-    #         {"text": "Edit the task", "action_text": "edit"},
-    #     ]
+    if await db.is_teacher(user_id):
+        buttons_list = [
+            {"text": "See solutions", "action_text": "show_solutions"},
+            {"text": "Edit the task", "action_text": "edit"},
+        ]
     keyboard = InlineKeyboardBuilder()
     keyboard.row(
         *[
