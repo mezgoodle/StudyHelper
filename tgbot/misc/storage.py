ad4a29b5a6ab361635566724f573ee9a2278daf7
Maksym Zavalniuk <mezgoodle@gmail.com>
2024-08-26 16:34:01 +0300

    Create bucket with client if not exists

--- tgbot/misc/storage.py
+++ tgbot/misc/storage.py
@@ -1,7 +1,7 @@
 import logging
 
 import boto3
-from botocore.exceptions import NoCredentialsError
+from botocore.exceptions import ClientError, NoCredentialsError
 
 
 class Storage:
@@ -13,20 +13,15 @@ class Storage:
         region_name: str,
     ):
         self.bucket_name = bucket_name
+        self.region_name = region_name
         try:
             self.client = boto3.client(
                 service_name="s3",
-                region_name=region_name,
+                region_name=self.region_name,
                 aws_access_key_id=access_id,
                 aws_secret_access_key=access_key,
             )
-            self.s3 = boto3.resource(
-                service_name="s3",
-                region_name="eu-central-1",
-                aws_access_key_id=access_id,
-                aws_secret_access_key=access_key,
-            )
-            self.set_bucket(self.bucket_name)
+            self.create_bucket_if_not_exists()
         except NoCredentialsError:
             logging.error("Credentials not found")
 
@@ -37,11 +32,22 @@ class Storage:
         except Exception as e:
             logging.error(f"Error while listing buckets: {e}")
 
-    def set_bucket(self, bucket_name):
+    def create_bucket_if_not_exists(self):
         try:
-            self.bucket = self.s3.Bucket(bucket_name)
-        except Exception as e:
-            logging.error(f"Error while setting bucket: {e}")
+            self.client.head_bucket(Bucket=self.bucket_name)
+            logging.info(f"Bucket {self.bucket_name} already exists.")
+        except ClientError as e:
+            error_code = int(e.response["Error"]["Code"])
+            if error_code == 404:
+                self.client.create_bucket(
+                    Bucket=self.bucket_name,
+                    CreateBucketConfiguration={
+                        "LocationConstraint": self.region_name
+                    },
+                )
+                logging.info(f"Bucket {self.bucket_name} created.")
+            else:
+                logging.error(f"Error occurred: {e}")
 
     def get_bucket(self):
         return self.bucket
