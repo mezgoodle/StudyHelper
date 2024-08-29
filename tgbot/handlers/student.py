9676419afc0ef0ced25a2588e161410223c814fd
Maksym Zavalniuk <mezgoodle@gmail.com>
2024-08-29 21:00:43 +0300

    Add condition if there are no tasks

--- tgbot/handlers/student.py
+++ tgbot/handlers/student.py
@@ -49,18 +49,24 @@ async def show_upcoming_tasks(
     subjects = await db.get_student_subjects(student)
     for subject in subjects:
         subject_text = f"Your tasks for {hbold(subject.name)}:"
-        tasks_texts = []
-        for task in await subject.tasks.all().order_by("due_date"):
-            solution = await db.solution.filter(
-                student_id=student.pk, subject_task_id=task.pk
-            ).first()
-            is_done = "✅" if solution else "❌"
-            text = f"* Name: {hbold(task.name)}. Due date: {hbold(task.due_date.strftime('%d/%m/%Y'))}. Is done: {is_done}"
-            if solution:
-                text += f" Your grade: {hbold(solution.grade)}"
-            tasks_texts.append(text)
-        tasks_text = "\n".join(tasks_texts)
-        await message.answer(subject_text + "\n" + tasks_text)
+        tasks = await subject.tasks.all().order_by("due_date")
+        if tasks:
+            tasks_texts = []
+            for task in await subject.tasks.all().order_by("due_date"):
+                solution = await db.solution.filter(
+                    student_id=student.pk, subject_task_id=task.pk
+                ).first()
+                is_done = "✅" if solution else "❌"
+                text = f"* Name: {hbold(task.name)}. Due date: {hbold(task.due_date.strftime('%d/%m/%Y'))}. Is done: {is_done}"
+                if solution:
+                    text += f" Your grade: {hbold(solution.grade)}"
+                tasks_texts.append(text)
+            tasks_text = "\n".join(tasks_texts)
+            await message.answer(subject_text + "\n" + tasks_text)
+        else:
+            await message.answer(
+                subject_text + "\nThere are no tasks for this subject."
+            )
     return None
 
 
